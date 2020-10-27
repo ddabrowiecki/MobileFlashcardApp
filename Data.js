a2518bfafb416953a99fb67d1194652522334dea
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2020-10-26 18:35:49 -0700

    First commit

---
+++ Data.js
@@ -0,0 +1,10 @@
+let decks = {
+    react: {
+        id: "react",
+        questions: [{ question: "What is a component?", answer: "A piece of React" }],
+    },
+    sailing: {
+        id: "sailing",
+        questions: [{ question: "What is a sail?", answer: "The go material" }],
+    },
+};
