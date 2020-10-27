8d89962ea69b01bb7553e9bbe0753532041402fe
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2020-10-26 20:57:01 -0700

    Create component files

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Deck from './components/Deck'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Deck />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Deck.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Deck () {
+    return(
+        <View>
+            <Text>
+                Question Text
+            </Text>
+            <Text>
+                Answer Text
+            </Text>
+        </View>
+    )
+}
