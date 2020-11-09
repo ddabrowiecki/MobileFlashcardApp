b94abe5ead98f26044e5bde6ba50bf7938812113
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2020-11-08 18:22:32 -0800

    Add AddQuestion component

--- components/AddQuestion.js
+++ components/AddQuestion.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import { connect } from "react-redux";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class AddCard extends React.Component {
+  state = {
+    questionInput: "",
+    answerInput: "",
+  };
+
   render() {
+    const { input } = this.props;
     return (
       <View>
         <Text>Question</Text>
+        <TextInput value={input}></TextInput>
         <Text>Answer</Text>
+        <TextInput value={input}></TextInput>
         <TouchableOpacity>
-        <Text style={styles.deckButton}>Submit</Text>
+          <Text style={styles.deckButton}>Submit</Text>
         </TouchableOpacity>
       </View>
     );
@@ -24,7 +32,7 @@ const styles = StyleSheet.create({
     borderColor: "red",
     borderWidth: 4,
     borderRadius: 6,
-    textAlign: "center", 
+    textAlign: "center",
     margin: 10,
     width: 250,
   },
